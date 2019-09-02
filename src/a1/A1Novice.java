e81ea4ca5db05c08b2db90b017a016b1f93ef7b1
ceciliaposton <ceciliaposton@vanessas-air.wireless-1x.unc.edu>
2019-09-01 23:19:45 -0400

    completed test 1

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -8,7 +8,39 @@ public class A1Novice {
 		
 		Scanner scan = new Scanner(System.in);
 
-		// Your code follows here.
+		// Read how many customers there are.
 		
+		int count = scan.nextInt();
+		
+		// Create an array to store each customer.
+		
+		String[] firstname = new String[count];
+		char[] firstinitials = new char[count];
+		String[] lastname = new String[count];
+		int[] amtbought = new int[count];
+		double[] totalcost = new double[count];
+		
+		// Put first names in array
+		for (int i=0; i < (firstname.length); i++) {
+			firstname[i] = scan.next();
+			lastname[i] = scan.next();
+			amtbought[i] = scan.nextInt();
+			for (int j=0; j < (amtbought[i]); j++) {
+				int[] itemsbought = new int[amtbought[i]];
+				String[] itembought = new String[amtbought[i]];
+				itemsbought[j] = scan.nextInt();
+				itembought[j] = scan.next();
+				totalcost[i] = totalcost[i] + scan.nextDouble();
+			}
+		}
+				
+		// Put first initials in array
+		for (int i=0; i<firstname.length; i++) {
+			firstinitials[i] = firstname[i].charAt(0);
+		}
+		
+		for (int i=0; i<firstname.length; i++) {
+		System.out.println(firstinitials[i] + ". " + lastname[i] + ": " + totalcost[i]);
+		}
 	}
 }
